7b2b270b578d23112bb0966ee69a18522978c243
Javi <ingenierodesoftware73@gmail.com>
2023-04-16 19:44:52 -0500

    header and logo

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>store-t</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <!-- header -->
+    <header class="header">
+        <div id="logo">
+            <img src="assets/img/tshirt.png" alt="tshirt logo" srcset="">
+            <a href="index.php">
+                <h1>Tshirt Store</h1>
+            </a>
+        </div>
+    </header>
+    <!-- menu -->
+
+    <!-- left menu -->
+
+    <!-- center content -->
+
+    <!-- footer -->
+</body>
+</html>
