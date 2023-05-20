98f169b0a023a522e1fd120610d8465d387cb453
j4vj4r <ingenierodesoftware73@gmail>
2023-05-20 17:25:23 -0500

    test 4

--- controller/UserController.php
+++ controller/UserController.php
@@ -18,7 +18,7 @@ class userController{
             $user->setEmail($_POST['email']);
             $user->setPassword($_POST['password']);
 
-            var_dump("test3");
+            var_dump("test4");
         }
     }
 }
